10fc2a47474b88ed041fb705e1880340891814d8
Forest Ho-Chen <foresth168@gmail.com>
2025-05-13 15:00:05 -0400

    Progress Bar Progress

--- Love_Sees_Differences/Assets/Scripts/CallableLyricsDisplay.cs
+++ Love_Sees_Differences/Assets/Scripts/CallableLyricsDisplay.cs
@@ -44,6 +44,8 @@ public class CallableLyricsDisplay : MonoBehaviour
     [SerializeField] private RectTransform lyricsProgressBarFill;
     private float originalLyricsBarWidth;
 
+    [SerializeField] private RectTransform lyricsLineFillBar;
+
 
     // Start is called before the first frame update
     void Start() {
@@ -70,6 +72,11 @@ public class CallableLyricsDisplay : MonoBehaviour
             StopCoroutine(doLyricsCoroutine);
             doLyricsCoroutine = null;
         }
+        if (lineFillCoroutine != null)
+            StopCoroutine(lineFillCoroutine);
+
+        lyricsLineFillBar.sizeDelta = new Vector2(0f, lyricsLineFillBar.sizeDelta.y);
+
     }
 
     public void OnNextPressed() {
@@ -147,6 +154,7 @@ public class CallableLyricsDisplay : MonoBehaviour
                 yield return new WaitForSecondsRealtime((float)waitTime);
 
             UpdateLyricsDisplay();
+            UpdateLyricsProgressBar(startLineFill: true);
             nextLyricTime += lyrics[currentLine].duration * secondsPerBeat;
             currentLine++;
         }
@@ -166,19 +174,47 @@ public class CallableLyricsDisplay : MonoBehaviour
         UpdateLyricsProgressBar();
     }
 
-    private void UpdateLyricsProgressBar() {
+    private void UpdateLyricsProgressBar(bool startLineFill = false) {
         float percentage = (float)currentLine / (lyrics.Count - 1);
+        float barWidth = percentage * originalLyricsBarWidth;
 
-        //Debug.Log(percentage);
+        lyricsProgressBarFill.sizeDelta = new Vector2(barWidth, lyricsProgressBarFill.sizeDelta.y);
 
-        lyricsProgressBarFill.sizeDelta = new Vector2(
-            percentage * originalLyricsBarWidth,
-            lyricsProgressBarFill.sizeDelta.y
-        );
+        if (startLineFill && currentLine < lyrics.Count) {
+            float segmentWidth = originalLyricsBarWidth / (lyrics.Count - 1);
+            float offsetX = segmentWidth * currentLine;
+
+            if (lineFillCoroutine != null)
+                StopCoroutine(lineFillCoroutine);
+
+            float lineDuration = lyrics[currentLine].duration * secondsPerBeat;
+
+            // Reset the fill width to 0
+            lyricsLineFillBar.sizeDelta = new Vector2(0f, lyricsLineFillBar.sizeDelta.y);
+
+            lineFillCoroutine = StartCoroutine(FillLineBar(lineDuration, segmentWidth, offsetX));
+        }
     }
 
     private void ResetLyricsProgressBar() {
         lyricsProgressBarFill.sizeDelta = new Vector2(0, lyricsProgressBarFill.sizeDelta.y);
+    }
+
+    private Coroutine lineFillCoroutine;
+
+    private IEnumerator FillLineBar(float duration, float segmentWidth, float offsetX) {
+        float elapsed = 0f;
+
+        while (elapsed < duration) {
+            float t = elapsed / duration;
+            lyricsLineFillBar.sizeDelta = new Vector2(segmentWidth * t, lyricsLineFillBar.sizeDelta.y);
+            lyricsLineFillBar.anchoredPosition = new Vector2(offsetX, lyricsLineFillBar.anchoredPosition.y);
+            elapsed += Time.unscaledDeltaTime;  // Use unscaled for Realtime
+            yield return null;
+        }
+
+        // Snap to full at the end
+        lyricsLineFillBar.sizeDelta = new Vector2(segmentWidth, lyricsLineFillBar.sizeDelta.y);
     }
 
 }
